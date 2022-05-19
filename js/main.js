f58c0114bf38b84da558d5cae6f0b282f20da499
Marius L <mariusleica@gmail.com>
2022-05-19 20:00:21 +0200

    Done bonuses

--- js/main.js
+++ js/main.js
@@ -13,26 +13,68 @@ generaButton.addEventListener("click", function () {
   if (inputName.value == "" || inputAge.value == "" || inputKm.value == "") {
     datiValidi = false;
     alert("Per favore inserisci tutti i dati richiesti");
-  }
+  } else {
+    let userName = inputName.value;
+    if (!isNaN(userName) || userName.length < 3) {
+      datiValidi = false;
+      alert("Per favore inserisci un valore valido per il nome");
+    }
 
-  let userName = inputName.value;
-  if (!isNaN(userName) || userName.length < 3) {
-    datiValidi = false;
-    alert("Per favore inserisci un valore valido per il nome");
-  }
-
-  if (inputAge.value < 14 || inputAge.value > 122) {
-    datiValidi = false;
-    alert("Per favore inserisci un valore compreso tra 14 e 122 anni");
-  }
-  if (inputKm.value < 10 || inputKm.value > 2000) {
-    datiValidi = false;
-    alert("Per favore inserisci un valore compreso tra 10 e 2000 km");
+    if (inputAge.value < 14 || inputAge.value > 122) {
+      datiValidi = false;
+      alert("Per favore inserisci un valore compreso tra 14 e 122 anni");
+    }
+    if (inputKm.value < 10 || inputKm.value > 2000) {
+      datiValidi = false;
+      alert("Per favore inserisci un valore compreso tra 10 e 2000 km");
+    }
   }
   if (datiValidi == true) {
-    console.log(inputName.value);
-    console.log(inputAge.value);
-    console.log(inputKm.value);
+    const prezzoAlKm = 0.21;
+
+    // creiamo una variabile dove calcolare il prezzo finale
+    let prezzoFinale = parseInt(inputKm.value) * prezzoAlKm;
+
+    // variabile per lo sconto
+    let scontoDaApplicare = 0;
+    console.log("prezzo originale: ", prezzoFinale);
+
+    if (parseInt(inputAge.value) < 18) {
+      // applico sconto 20%
+      scontoDaApplicare = 0.2;
+
+      //  prezzoFinale = prezzoFinale - prezzoFinale * 0.2;
+    } else if (parseInt(inputAge.value) >= 65) {
+      // applico sconto 40%
+      scontoDaApplicare = 0.4;
+
+      // prezzoFinale = prezzoFinale - prezzoFinale * 0.4;
+    }
+
+    //  prezzoFinale = prezzoFinale - prezzoFinale * scontoDaApplicare;
+    // Sarebbe coretto da scrivere cosi:
+    prezzoFinale -= prezzoFinale * scontoDaApplicare;
+
+    // Stampo il prezzo finale
+    const customDisplayElement = document.getElementById("display-custom");
+
+    customDisplayElement.classList.add("d-block");
+    document.getElementById("prezzo").innerHTML = prezzoFinale.toFixed(2);
+    document.getElementById("user-name").innerHTML = inputName.value;
+    if (inputAge.value < 18 || inputAge.value > 65) {
+      document.getElementById("sconto").innerHTML = "Biglietto ridotto";
+    } else {
+      document.getElementById("sconto").innerHTML = "Biglietto standard";
+      let car = Math.floor(Math.random() * 101);
+      document.getElementById("carrozza").innerHTML = car;
+      document.getElementById("CP").innerHTML =
+        Math.floor(Math.random() * 99999) + 10000;
+
+      document.getElementById("prezzo").innerHTML =
+        prezzoFinale.toFixed(2) + " " + "€";
+
+      console.log("prezzo finale: ", prezzoFinale);
+    }
   }
 });
 
@@ -41,4 +83,6 @@ annulaButton.addEventListener("click", function () {
   inputName.value = "";
   inputAge.value = "";
   inputKm.value = "";
+  let customDisplayElement = document.getElementById("display-custom");
+  customDisplayElement.classList.remove("d-block");
 });
